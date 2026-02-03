b8b755ffe4e01e569e958c468db8760899bc9362
Hridoy Munda <hridoymnd10@gmail.com>
2026-02-03 07:45:30 -0800

    added addNumbers function

--- parameter.js
+++ parameter.js
@@ -7,3 +7,12 @@ squareKoro(2);
 squareKoro(3);
 squareKoro(4);
 squareKoro(5);
+
+
+// adding function
+function addNumbers (num1, num2) {
+    const total = num1 + num2;
+    console.log(total)
+};
+
+addNumbers(4, 5);
